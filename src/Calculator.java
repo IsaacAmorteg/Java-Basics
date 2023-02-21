2783d2fe4ed2c2cec8e3bf5a093ac734c004768f
BOG-615 <BOG-615@BOG-615.BOG-615>
2023-02-21 18:32:12 -0500

    First Commit

---
+++ src/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        System.out.println("Welcome to the Calculator.");
+    }
+}
---
+++ src/Pets.java
@@ -0,0 +1,5 @@
+public class Pets {
+    public static void main(String[] args) {
+
+    }
+}
