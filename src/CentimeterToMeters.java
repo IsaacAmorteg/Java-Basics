4845529843a8c6f82b9554021f0df0971eb8d25a
Isaac Amortegui <Isaacamortegc@outlook.com>
2023-02-24 17:06:45 -0500

    Centimeters To Meters function V1.0

---
+++ src/CentimeterToMeters.java
@@ -0,0 +1,9 @@
+public class CentimeterToMeters {
+    public static void main(String[] args) {
+        System.out.println("Please enter the number in Centimeters");
+        double centimeters = 185;
+        double meters = centimeters * 0.01;
+        System.out.println(centimeters + " centimeters are equal to " + meters + " meters.");
+
+    }
+}
